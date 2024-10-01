d8ec4aa27063eee9306330733650da03747473ae
srksifat-dev <srksifat.dev@gmail.com>
2024-10-01 14:00:58 +0600

    Initial Commit

---
+++ RockPaperScissor/RockPaperScissorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorApp.swift
+//  RockPaperScissor
+//
+//  Created by Md. Shoibur Rahman Khan Sifat on 10/1/24.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
